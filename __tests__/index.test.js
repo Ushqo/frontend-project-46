344b5c47ceccb2ffdb310681731fceb16540b49a
Dmitriy Ushakov <Ush.Dmitriy@yandex.ru>
2023-05-03 15:53:04 +0400

    Fix naming in index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,20 +8,20 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('compare json files', () => {
-  const fileToCompare1 = getFixturePath('file1.json');
-  const fileToCompare2 = getFixturePath('file2.json');
+  const pathTofileToCompare1 = getFixturePath('file1.json');
+  const pathTofileToCompare2 = getFixturePath('file2.json');
 
-  const actual = genDiff(fileToCompare1, fileToCompare2);
+  const actual = genDiff(pathTofileToCompare1, pathTofileToCompare2);
   const expected = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
 
   expect(actual).toEqual(expected);
 });
 
 test('compare yaml files', () => {
-  const fileToCompare1 = getFixturePath('file1.yml');
-  const fileToCompare2 = getFixturePath('file2.yml');
+  const pathTofileToCompare1 = getFixturePath('file1.yml');
+  const pathTofileToCompare2 = getFixturePath('file2.yml');
 
-  const actual = genDiff(fileToCompare1, fileToCompare2);
+  const actual = genDiff(pathTofileToCompare1, pathTofileToCompare2);
   const expected = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
 
   expect(actual).toEqual(expected);
