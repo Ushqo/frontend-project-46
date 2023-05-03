cfe5e3bc1b4dbe8a50defcd3facb41b9b7ba2388
Dmitriy Ushakov <Ush.Dmitriy@yandex.ru>
2023-05-03 13:51:16 +0400

    Refactor index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,9 @@
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import path from 'path';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
