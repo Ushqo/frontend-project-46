805aefbafab46ab2e282ad930992e60799cb4277
Dmitriy Ushakov <Ush.Dmitriy@yandex.ru>
2023-05-03 13:23:19 +0400

    Feat add tests

---
+++ __tests__/index.test.js
@@ -0,0 +1,19 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('compare json files', () => {
+  const fileToCompare1 = getFixturePath('file1.json');
+  const fileToCompare2 = getFixturePath('file2.json');
+
+  const actual = genDiff(fileToCompare1, fileToCompare2);
+  const expected = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+
+  expect(actual).toEqual(expected);
+});
