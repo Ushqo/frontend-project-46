becec1551f81f7c5d5e55da6e0bb923b2a1d02e3
Dmitriy Ushakov <Ush.Dmitriy@yandex.ru>
2023-05-08 22:12:01 +0400

    Refactor genDiff.js

--- src/genDiff.js
+++ src/genDiff.js
@@ -3,39 +3,20 @@ import _ from 'lodash';
 const genDiff = (obj1, obj2) => _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)))
   .map((key) => {
     if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      return {
-        key,
-        status: 'tree',
-        children: genDiff(obj1[key], obj2[key]),
-      };
+      return { key, status: 'tree', children: genDiff(obj1[key], obj2[key]) };
     }
     if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
-      return {
-        key,
-        status: 'added',
-        value: obj2[key],
-      };
+      return { key, status: 'added', value: obj2[key] };
     }
     if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
-      return {
-        key,
-        status: 'removed',
-        value: obj1[key],
-      };
+      return { key, status: 'removed', value: obj1[key] };
     }
     if ((obj1[key] !== obj2[key])) {
       return {
-        key,
-        status: 'updated',
-        newValue: obj2[key],
-        oldValue: obj1[key],
+        key, status: 'updated', newValue: obj2[key], oldValue: obj1[key],
       };
     }
-    return {
-      key,
-      status: 'not updated',
-      value: obj1[key],
-    };
+    return { key, status: 'not updated', value: obj1[key] };
   });
 
 export default genDiff;
