b698fad307ecca8f0aa6966a96b80ad8d4693366
Dmitriy Ushakov <Ush.Dmitriy@yandex.ru>
2023-05-11 20:14:39 +0400

    Refactor index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,62 +9,31 @@ const __dirname = dirname(__filename);
 const getFixturePath = (fileName) => resolve(__dirname, '..', '__fixtures__', fileName);
 const getExpected = (fileName) => readFileSync(getFixturePath(fileName), { encoding: 'utf8', flag: 'r' });
 
-test('compare json files with stylish formatter', () => {
-  const pathTofileToCompare1 = getFixturePath('file1.json');
-  const pathTofileToCompare2 = getFixturePath('file2.json');
-
-  const actual = genDiff(pathTofileToCompare1, pathTofileToCompare2);
-  const expected = getExpected('result-stylish.txt');
-
-  expect(actual).toEqual(expected);
-});
-
-test('compare yaml files with stylish formatter', () => {
-  const pathTofileToCompare1 = getFixturePath('file1.yml');
-  const pathTofileToCompare2 = getFixturePath('file2.yml');
-
-  const actual = genDiff(pathTofileToCompare1, pathTofileToCompare2);
-  const expected = getExpected('result-stylish.txt');
-
-  expect(actual).toEqual(expected);
-});
-
-test('compare json files with plain formatter', () => {
-  const pathTofileToCompare1 = getFixturePath('file1.json');
-  const pathTofileToCompare2 = getFixturePath('file2.json');
-
-  const actual = genDiff(pathTofileToCompare1, pathTofileToCompare2, 'plain');
-  const expected = getExpected('result-plain.txt');
-
-  expect(actual).toEqual(expected);
-});
-
-test('compare yaml files with plain formatter', () => {
-  const pathTofileToCompare1 = getFixturePath('file1.yml');
-  const pathTofileToCompare2 = getFixturePath('file2.yml');
-
-  const actual = genDiff(pathTofileToCompare1, pathTofileToCompare2, 'plain');
-  const expected = getExpected('result-plain.txt');
-
-  expect(actual).toEqual(expected);
-});
-
-test('compare json files with json formatter', () => {
-  const pathTofileToCompare1 = getFixturePath('file1.json');
-  const pathTofileToCompare2 = getFixturePath('file2.json');
-
-  const actual = genDiff(pathTofileToCompare1, pathTofileToCompare2, 'json');
-  const expected = getExpected('result-json.txt');
-
-  expect(actual).toEqual(expected);
-});
-
-test('compare yaml files with json formatter', () => {
-  const pathTofileToCompare1 = getFixturePath('file1.yml');
-  const pathTofileToCompare2 = getFixturePath('file2.yml');
-
-  const actual = genDiff(pathTofileToCompare1, pathTofileToCompare2, 'json');
-  const expected = getExpected('result-json.txt');
-
+test.each([
+  {
+    fileName1: 'file1.json', fileName2: 'file2.json', formatter: 'stylish', expectedFileName: 'result-stylish.txt',
+  },
+  {
+    fileName1: 'file1.yml', fileName2: 'file2.yml', formatter: 'stylish', expectedFileName: 'result-stylish.txt',
+  },
+  {
+    fileName1: 'file1.json', fileName2: 'file2.json', formatter: 'plain', expectedFileName: 'result-plain.txt',
+  },
+  {
+    fileName1: 'file1.yml', fileName2: 'file2.yml', formatter: 'plain', expectedFileName: 'result-plain.txt',
+  },
+  {
+    fileName1: 'file1.json', fileName2: 'file2.json', formatter: 'json', expectedFileName: 'result-json.txt',
+  },
+  {
+    fileName1: 'file1.yml', fileName2: 'file2.yml', formatter: 'json', expectedFileName: 'result-json.txt',
+  },
+])(('compare $fileName1 and $fileName2 with $formatter formatter'), ({
+  fileName1, fileName2, formatter, expectedFileName,
+}) => {
+  const path1 = getFixturePath(fileName1);
+  const path2 = getFixturePath(fileName2);
+  const actual = genDiff(path1, path2, formatter);
+  const expected = getExpected(expectedFileName);
   expect(actual).toEqual(expected);
 });
