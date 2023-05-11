e1692138e27c1978b6ee0274240e71f186b1e312
Dmitriy Ushakov <Ush.Dmitriy@yandex.ru>
2023-05-11 20:22:30 +0400

    Fix genDiff.js

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,22 +1,31 @@
 import _ from 'lodash';
 
-const genDiff = (obj1, obj2) => _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)))
-  .map((key) => {
+const genDiff = (obj1, obj2) => {
+  const objKeys1 = Object.keys(obj1);
+  const objKeys2 = Object.keys(obj2);
+
+  const uniqueKeys = _.union(objKeys1, objKeys2);
+  const sortedKeys = _.sortBy(uniqueKeys);
+
+  const difference = sortedKeys.map((key) => {
     if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      return { key, status: 'tree', children: genDiff(obj1[key], obj2[key]) };
+      return { key, type: 'nested', children: genDiff(obj1[key], obj2[key]) };
     }
     if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
-      return { key, status: 'added', value: obj2[key] };
+      return { key, type: 'added', value: obj2[key] };
     }
     if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
-      return { key, status: 'removed', value: obj1[key] };
+      return { key, type: 'removed', value: obj1[key] };
     }
-    if ((obj1[key] !== obj2[key])) {
+    if (!_.isEqual(obj1[key], obj2[key])) {
       return {
-        key, status: 'updated', newValue: obj2[key], oldValue: obj1[key],
+        key, type: 'updated', value2: obj2[key], value1: obj1[key],
       };
     }
-    return { key, status: 'not updated', value: obj1[key] };
+    return { key, type: 'notUpdated', value: obj1[key] };
   });
+
+  return difference;
+};
 
 export default genDiff;
